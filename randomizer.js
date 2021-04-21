fdc3f32b232446046924e11b67fce36f8d451da3
michalkrajka <82782527+michalkrajka@users.noreply.github.com>
2021-04-21 02:29:37 +0200

    Add files via upload

---
+++ randomizer.js
@@ -0,0 +1,91 @@
+
+
+function random1()
+{
+    var liczba = Math.floor(Math.random()*17)+1;
+
+    if (liczba==1)liczba="EMF Reader";
+   else if (liczba==2)liczba="Ghost Writing Book";
+   else if (liczba==3)liczba="Spirit Box";
+   else if (liczba==4)liczba="Thermometer";
+   else if (liczba==5)liczba="Video Camera";
+   else if (liczba==6)liczba="Photo Camera";
+   else if (liczba==7)liczba="Crucifix";
+   else if (liczba==8)liczba="Glow Stick";
+   else if (liczba==9)liczba="Head Mounted Camera";
+   else if (liczba==10)liczba="Infrared Light Sensor";
+   else if (liczba==11)liczba="Motion Sensor";
+   else if (liczba==12)liczba="Parabolic Microphone";
+   else if (liczba==13)liczba="Salt Shaker";
+   else if (liczba==14)liczba="Sanity Pills";
+   else if (liczba==15)liczba="Smudge Sticks";
+   else if (liczba==16)liczba="Sound Sensor";
+   else if (liczba==17)liczba="Tripod";
+
+    
+    document.getElementById("item1").innerHTML=liczba;
+}
+
+function random2()
+{
+    var liczba = Math.floor(Math.random()*17)+1;
+
+    if (liczba==1)liczba="EMF Reader";
+   else if (liczba==2)liczba="Ghost Writing Book";
+   else if (liczba==3)liczba="Spirit Box";
+   else if (liczba==4)liczba="Thermometer";
+   else if (liczba==5)liczba="Video Camera";
+   else if (liczba==6)liczba="Photo Camera";
+   else if (liczba==7)liczba="Crucifix";
+   else if (liczba==8)liczba="Glow Stick";
+   else if (liczba==9)liczba="Head Mounted Camera";
+   else if (liczba==10)liczba="Infrared Light Sensor";
+   else if (liczba==11)liczba="Motion Sensor";
+   else if (liczba==12)liczba="Parabolic Microphone";
+   else if (liczba==13)liczba="Salt Shaker";
+   else if (liczba==14)liczba="Sanity Pills";
+   else if (liczba==15)liczba="Smudge Sticks";
+   else if (liczba==16)liczba="Sound Sensor";
+   else if (liczba==17)liczba="Tripod";
+
+    
+    document.getElementById("item2").innerHTML=liczba;
+}
+
+function random3()
+{
+    var liczba = Math.floor(Math.random()*17)+1;
+
+    if (liczba==1)liczba="EMF Reader";
+   else if (liczba==2)liczba="Ghost Writing Book";
+   else if (liczba==3)liczba="Spirit Box";
+   else if (liczba==4)liczba="Thermometer";
+   else if (liczba==5)liczba="Video Camera";
+   else if (liczba==6)liczba="Photo Camera";
+   else if (liczba==7)liczba="Crucifix";
+   else if (liczba==8)liczba="Glow Stick";
+   else if (liczba==9)liczba="Head Mounted Camera";
+   else if (liczba==10)liczba="Infrared Light Sensor";
+   else if (liczba==11)liczba="Motion Sensor";
+   else if (liczba==12)liczba="Parabolic Microphone";
+   else if (liczba==13)liczba="Salt Shaker";
+   else if (liczba==14)liczba="Sanity Pills";
+   else if (liczba==15)liczba="Smudge Sticks";
+   else if (liczba==16)liczba="Sound Sensor";
+   else if (liczba==17)liczba="Tripod";
+
+    
+    document.getElementById("item3").innerHTML=liczba;
+}
+
+function random4()
+{
+    var liczba = Math.floor(Math.random()*4)+1;
+
+    if (liczba==1)liczba="Candle";
+   else if (liczba==2)liczba="Flashlight";
+   else if (liczba==3)liczba="Strong Flashlight";
+   else if (liczba==4)liczba="UV Flashlight";
+    
+    document.getElementById("item4").innerHTML=liczba;
+}
